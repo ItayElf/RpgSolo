4cec6c102c731251a2f210ac0b21acdda1bc97ed
ItayElf <thh123321@gmail.com>
2022-08-05 22:10:01 +0300

    created generator hub

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rpgsolo/components/sidebar.dart';
+import 'package:rpgsolo/pages/generate_pages/generate_npc.dart';
+import 'package:rpgsolo/utils/extensions.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -14,11 +16,69 @@ class Home extends StatelessWidget {
       drawer: const SideBar(
         pageId: 0,
       ),
-      body: Center(
-        child: Text(
-          "RpgSolo - Home",
-          style: Theme.of(context).textTheme.headline4,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  "Welcome to RpgSolo!",
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Center(
+                child: Text(
+                  "What would you like to generate?",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              GridView.count(
+                physics: const ScrollPhysics(),
+                shrinkWrap: true,
+                crossAxisCount: 3,
+                childAspectRatio: 1,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                children: const [
+                  GeneratorButton(
+                    title: "NPC",
+                    moveTo: GenerateNpc(),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class GeneratorButton extends StatelessWidget {
+  const GeneratorButton({super.key, required this.title, required this.moveTo});
+
+  final String title;
+  final Widget moveTo;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () =>
+          Navigator.of(context).push(SlideRoute(builder: (context) => moveTo)),
+      child: Text(
+        title,
+        style: Theme.of(context)
+            .textTheme
+            .headline6
+            ?.copyWith(color: Colors.white),
       ),
     );
   }
