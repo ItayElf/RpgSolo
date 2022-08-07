2ffa8a27152aba671f14bfe956b9bcf874850c3d
ItayElf <thh123321@gmail.com>
2022-08-07 19:30:28 +0300

    ask the oracle

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rpgsolo/pages/Home.dart';
+import 'package:rpgsolo/pages/ask_the_oracle.dart';
 import 'package:rpgsolo/pages/roll_page.dart';
 import 'package:rpgsolo/utils/extensions.dart';
 
@@ -28,59 +29,85 @@ class SideBar extends StatelessWidget {
               ),
             ),
           ),
-          ListTile(
-            leading: Icon(
-              Icons.auto_awesome,
-              color: Theme.of(context).primaryColor,
-            ),
-            title: Text(
-              "Generators",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            onTap: () {
-              if (pageId != 0) {
-                Navigator.of(context).pushReplacement(
-                    SlideRoute(builder: ((context) => const Home())));
-              }
-            },
+          SideTile(
+            pageId: pageId,
+            title: "Generators",
+            icon: Icons.auto_awesome,
+            moveTo: const Home(),
+            index: 0,
           ),
           Divider(
             color: Theme.of(context).primaryColor,
-            thickness: 2,
+            thickness: 1,
           ),
-          ListTile(
-            leading: Icon(
-              Icons.casino,
-              color: Theme.of(context).primaryColor,
-            ),
-            title: Text(
-              "Dice Roller",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            onTap: () {
-              if (pageId != 1) {
-                Navigator.of(context).pushReplacement(
-                    SlideRoute(builder: ((context) => const RollPage())));
-              }
-            },
+          SideTile(
+            pageId: pageId,
+            title: "Dice Roller",
+            icon: Icons.casino,
+            moveTo: const RollPage(),
+            index: 1,
           ),
           Divider(
             color: Theme.of(context).primaryColor,
-            thickness: 2,
+            thickness: 1,
           ),
-          ListTile(
-            leading: Icon(
-              Icons.star,
-              color: Theme.of(context).primaryColor,
-            ),
-            title: Text(
-              "Saved Items",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            onTap: () {},
+          SideTile(
+            pageId: pageId,
+            title: "Ask the Oracle",
+            icon: Icons.self_improvement,
+            moveTo: const AskTheOracle(),
+            index: 2,
+          ),
+          Divider(
+            color: Theme.of(context).primaryColor,
+            thickness: 1,
+          ),
+          SideTile(
+            pageId: pageId,
+            title: "Saved Items",
+            icon: Icons.star,
+            moveTo: null,
+            index: 3,
           ),
         ],
       ),
+    );
+  }
+}
+
+class SideTile extends StatelessWidget {
+  const SideTile({
+    super.key,
+    required this.pageId,
+    required this.title,
+    required this.icon,
+    required this.moveTo,
+    required this.index,
+  });
+
+  final int pageId;
+  final int index;
+  final String title;
+  final IconData icon;
+  final Widget? moveTo;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: Theme.of(context).primaryColor,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      onTap: () {
+        if (pageId != index && moveTo != null) {
+          Navigator.of(context).pushReplacement(
+              SlideRoute(builder: ((context) => moveTo as Widget)));
+        }
+      },
     );
   }
 }
---
+++ lib/pages/ask_the_oracle.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:rpgsolo/components/sidebar.dart';
+import 'package:rpgsolo/utils/dice.dart';
+
+class AskTheOracle extends StatefulWidget {
+  const AskTheOracle({super.key});
+
+  @override
+  State<AskTheOracle> createState() => _AskTheOracleState();
+}
+
+class _AskTheOracleState extends State<AskTheOracle> {
+  String? answer = "?";
+
+  final odds = [
+    "Almost Certain",
+    "Likely",
+    "Equaly Likely",
+    "Unlikely",
+    "Small Chance"
+  ];
+  final thresholds = [11, 26, 51, 76, 91];
+
+  void onClick(int i) {
+    setState(() {
+      answer = null;
+    });
+    Dice d = Dice(diceCount: 1, diceSize: 100);
+    int res = d.roll()[0];
+    String ans = res >= thresholds[i] ? "Yes" : "No";
+    if (res ~/ 10 == res % 10) {
+      ans = "Exceptional $ans";
+    }
+    Future.delayed(
+      const Duration(milliseconds: 500),
+      () {
+        setState(() {
+          answer = ans;
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ask the Oracle"),
+        centerTitle: true,
+      ),
+      drawer: const SideBar(
+        pageId: 2,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                "Ask the Oracle",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            Center(
+              child: Text(
+                "Choose the odds of the event:",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: odds.length,
+              itemBuilder: ((context, i) => ElevatedButton(
+                    onPressed: () {
+                      onClick(i);
+                    },
+                    child: Text(
+                      odds[i],
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          ?.copyWith(color: Colors.white),
+                    ),
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Answer:",
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            Center(
+              child: answer != null
+                  ? FittedBox(
+                      fit: BoxFit.fitWidth,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          answer ?? "",
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline3
+                              ?.copyWith(color: Colors.black),
+                        ),
+                      ),
+                    )
+                  : SizedBox(
+                      height: 64,
+                      width: 64,
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).primaryColor,
+                        strokeWidth: 6,
+                      ),
+                    ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
